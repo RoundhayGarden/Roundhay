be83a864059996554f3304371ef1edcb47a82f92
ahmedhatemkhalil <ahmedhatemkhalil@gmail.com>
2026-04-23 14:28:20 +0200

    feat(header): implement header UI

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header"
+
 const MainLayout = ({ children }) => {
   return (
     <div className="container-page">
+      <Header />
       <main>{children}</main>
     </div>
   )
